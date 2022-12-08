1331ad2ad081633e76f29f723fd77d9a18c30ed5
tangzihan8941 <wy_tangzihan@163.com>
2022-12-08 12:17:12 +0800

    12.7 add filedialog

--- Windows/test.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import filedialog
-
-root = tk.Tk()
-root.overrideredirect(True)
-root.attributes("-alpha", 0)
-path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=(("Text Documents", "*.txt"),))
-root.destroy()
