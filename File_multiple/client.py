04d9670ab75191af3f80479de30ea01938f63fbb
芋圆小丸子 <wangyanqhdd@163.com>
2022-12-10 18:37:43 +0800

    12.10 multiple file

---
+++ File_multiple/client.py
@@ -0,0 +1,71 @@
+import ctypes
+import json
+import os
+import struct
+from socket import *
+from threading import Thread
+
+from PyQt5.QtCore import QThread, pyqtSignal
+
+FILEPATH = "E:/test/"
+
+class Client():
+    receiveStartSignal = pyqtSignal()
+    receiveEndSignal = pyqtSignal(str)
+
+    def __init__(self, ip, openPort, ipToConnect, portToConnect):
+        super().__init__()
+        self.openPort = openPort
+        self.ipToConnect = ipToConnect
+        self.portToConnect = portToConnect
+        self.ip = ip
+
+    def client_socket(self):
+        # 创建客户端
+        self.clint = socket(AF_INET, SOCK_STREAM)
+        ip_port = (self.ipToConnect, self.portToConnect)
+        self.clint.connect(ip_port)
+        print("{0} connecting...".format(self.ip))
+        # 接收文件
+        # while True:
+        # 接收客户端发送的报头长度
+        buffSize = 1024
+        head_struct = self.clint.recv(4)
+        # 解析报头的长度
+        head_len = struct.unpack('i', head_struct)[0]
+        # 接收大小为head_len的报头内容（报头内容包括文件大小，文件名内容）
+        data = self.clint.recv(head_len)
+        # self.receiveStartSignal.emit()
+        # 解析报头的内容, 报头为一个字典其中包含上传文件的大小和文件名，
+        head_dir = json.loads(data.decode("utf-8"))  # 将JSON字符串解码为python对象
+        filesize_b = head_dir["fileSize"]
+        fileName = head_dir["fileName"]
+        # 接收真实的文件内容
+        recv_len = 0
+        recv_mesg = b''
+        # 在服务器文件夹中创建新文件
+        fileInfor = FILEPATH + fileName
+        f = open(fileInfor, "wb")
+        # 开始接收用户上传的文件
+        while recv_len < filesize_b:
+            if filesize_b - recv_len > buffSize:
+                # 假设未上传的文件数据大于最大传输数据
+                recv_mesg = self.clint.recv(buffSize)
+                f.write(recv_mesg)
+                recv_len += len(recv_mesg)
+                print(f"{recv_len / filesize_b}")
+            else:
+                # 需要传输的文件数据小于最大传输数据大小
+                recv_mesg = self.clint.recv(filesize_b - recv_len)
+                recv_len += len(recv_mesg)
+                f.write(recv_mesg)
+                f.close()
+                print("文件接收完毕！")
+        # 向用户发送信号，文件已经上传完毕
+        completed = "1"
+        self.clint.send(bytes(completed, "utf-8"))
+        # self.receiveEndSignal.emit(fileName)
+
+if __name__ == '__main__':
+    receiver = Client("192.168.202.152", 22222, "192.168.202.152", 22222)
+    receiver.client_socket()
---
+++ File_multiple/server.py
@@ -0,0 +1,61 @@
+import ctypes
+import json
+import os
+import struct
+from socket import *
+from threading import Thread
+
+# 服务端 发送
+class Server():
+    def __init__(self, filename, ip, openPort, ipToConnect, portToConnect):
+        super().__init__()
+        self.filename = filename
+        self.openPort = openPort
+        self.ipToConnect = ipToConnect
+        self.portToConnect = portToConnect
+        self.ip = ip
+
+    def server_socket(self):
+        try:
+            self.sever = socket(AF_INET, SOCK_STREAM)
+            ip_port = ("", self.openPort)
+            # 监听
+            self.sever.bind(ip_port)
+            self.sever.listen()
+            self.severSock, addr = self.sever.accept()
+            Thread(target=self.send).start()
+        except:
+            pass
+
+    def send(self):
+        buffSize = 1024
+        # 上传文件
+        fileInfor = self.filename
+        num = fileInfor.rfind('\\')
+        fileName = fileInfor[num + 1:]
+        # 得到文件的大小
+        filesize_bytes = os.path.getsize(fileInfor)
+        # 创建复制文件
+        fileName = "new" + fileName
+        # 创建字典用于报头
+        dirc = {"fileName": fileName,
+                "fileSize": filesize_bytes}
+        # 将字典转为JSON字符，再将字符串的长度打包
+        head_infor = json.dumps(dirc)
+        head_infor_len = struct.pack('i', len(head_infor))
+        # 先发送报头长度，然后发送报头内容
+        self.severSock.send(head_infor_len)
+        self.severSock.send(head_infor.encode("utf-8"))
+        # 发送真实文件
+        with open(fileInfor, 'rb') as f:
+            data = f.read()
+            self.severSock.sendall(data)
+            f.close()
+        # 客户端若接受完文件会发送信号，服务端接收
+        completed = self.severSock.recv(buffSize).decode("utf-8")
+        if completed == "1":
+            print("发送成功")
+
+if __name__ == '__main__':
+    sender = Server(r'E:\test\123\1235.txt',"192.168.202.152", 22222, "192.168.202.152", 22222)
+    sender.server_socket()
