937d9ca9fe51ff3d3d030848b3116a0daf880462
DoubleZeroWater <processing.math@gmail.com>
2022-12-10 20:20:29 +0800

    AudioMulti feature.

---
+++ audio/AudioMultiClient.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# create time    : 2021-01-06 15:52
+# author  : CY
+# file    : voice_client.py
+# modify time:
+import socket
+import threading
+
+import pyaudio
+from PyQt5.QtCore import QThread
+
+
+class AudioMultiClient(QThread):
+    def __init__(self, ipToConnect, portToConnect):
+        super().__init__()
+        chunk_size = 1024  # 512
+        audio_format = pyaudio.paInt16
+        channels = 1
+        rate = 20000
+        self.CloseSign = False
+        self.ipToConnect = ipToConnect
+        self.portToConnect = portToConnect
+        self.p = pyaudio.PyAudio()
+        self.playing_stream = self.p.open(format=audio_format, channels=channels, rate=rate, output=True,
+                                          frames_per_buffer=chunk_size)
+        self.recording_stream = self.p.open(format=audio_format, channels=channels, rate=rate, input=True,
+                                            frames_per_buffer=chunk_size)
+
+        threading.Thread(target=self.receiveServerData).start()
+        threading.Thread(target=self.sendDataToServer).start()
+
+    def startConnecting(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.ipToConnect, self.portToConnect))
+        threading.Thread(target=self.receiveServerData).start()
+        threading.Thread(target=self.sendDataToServer).start()
+
+    def receiveServerData(self):
+        while not self.CloseSign:
+            try:
+                data = self.client.recv(1024)
+                self.playing_stream.write(data)
+            except:
+                pass
+
+    def sendDataToServer(self):
+        while not self.CloseSign:
+            try:
+                data = self.recording_stream.read(1024)
+                self.client.sendall(data)
+            except:
+                pass
+
+    def close(self):
+        self.CloseSign = True
+        self.client.close()
+
+
+if __name__ == "__main__":
+    AudioMultiClient("127.0.0.1", 22222)
---
+++ audio/AudioMultiServer.py
@@ -0,0 +1,81 @@
+from socket import *
+from threading import Thread
+
+import pyaudio
+from PyQt5.QtCore import QThread
+
+
+class AudioMultiServer(QThread):  # 发送声音
+
+    def __init__(self, openPort):
+        super().__init__()
+        self.openPort = openPort  # 我要开放的端口
+        self.socketList = []
+        # 以下是收发声音要用的变量
+        chunk_size = 1024  # 512
+        audio_format = pyaudio.paInt16
+        channels = 1
+        rate = 20000
+        self.closeSign = False
+        self.p = pyaudio.PyAudio()
+        self.recording_stream = self.p.open(format=audio_format, channels=channels, rate=rate, input=True,
+                                            frames_per_buffer=chunk_size)
+        self.playing_stream = self.p.open(format=audio_format, channels=channels, rate=rate, output=True,
+                                          frames_per_buffer=chunk_size)
+
+        Thread(target=self.sendMyMessage).start()
+        self.startListening()
+
+    def startListening(self):  # 发送声音
+        self.server = socket(AF_INET, SOCK_STREAM)
+        ip_port = ("", self.openPort)  # 开放openport等待iptoconnect来连接
+        self.server.bind(ip_port)
+        self.server.listen()
+        Thread(target=self.repeatGetConnection).start()
+
+    def repeatGetConnection(self):
+        try:
+            while not self.closeSign:
+                (self.conn, self.addr) = self.server.accept()
+                self.socketList.append((self.conn, self.addr))
+                Thread(target=self.receiveMessage,
+                       args=(self.socketList[-1],)).start()
+        except OSError:
+            print("You have END your server.")
+
+    def receiveMessage(self, socket):
+        try:
+            while not self.closeSign:
+                data = socket.recv(1024)
+                self.tellMyself(data)
+                self.broadcastExceptOne(data, socket)
+        except OSError:
+            print("This thread has been ended.")
+
+    def broadcastAllSocket(self, message):
+        for socket in self.socketList:
+            socket[0].sendall(message)
+
+    def broadcastExceptOne(self, message, socket):
+        for s in self.socketList:
+            if s != socket:
+                s[0].sendall(message)
+
+    def tellMyself(self, message):
+        self.playing_stream.write(message)
+
+    def sendMyMessage(self):
+        while not self.closeSign:
+            try:
+                data = self.recording_stream.read(1024)
+                self.broadcastAllSocket(data)
+            except:
+                pass
+
+    def close(self):
+        self.closeSign = True
+        self.server.close()
+
+
+if __name__ == "__main__":
+    AudioMultiServer(22222)
